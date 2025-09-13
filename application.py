ab681d6f2103b8e4334337e63e3a03974cf4f129
deekshithashetty746-ctrl <deekshithashetty746@gmail.com>
2025-09-13 21:22:02 +0530

    Update application.py

--- application.py
+++ application.py
@@ -3,6 +3,7 @@ from os import environ
 from FlaskWebProject import app
 
 if __name__ == '__main__':
+    # Azure App Service expects 0.0.0.0:8000
     HOST = '0.0.0.0'
-    PORT = int(environ.get('PORT', 8000))  # Azure expects 8000
+    PORT = int(environ.get('PORT', 8000))
     app.run(HOST, PORT)
