a88d636f085789495cfb7a903ce836bb2a3c72ba
deekshithashetty746-ctrl <deekshithashetty746@gmail.com>
2025-09-13 21:07:07 +0530

    Update models.py

--- FlaskWebProject/models.py
+++ FlaskWebProject/models.py
@@ -1,16 +1,15 @@
+
 from datetime import datetime
 from FlaskWebProject import app, db, login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
-from azure.storage.blob import BlobServiceClient, ContentSettings
+from azure.storage.blob import BlockBlobService
 import string, random
 from werkzeug.utils import secure_filename
 from flask import flash
 
-# Initialize Blob Storage Client
-blob_connection_string = app.config.get('BLOB_CONNECTION_STRING')
-blob_service = BlobServiceClient.from_connection_string(blob_connection_string)
-blob_container_client = blob_service.get_container_client(app.config['BLOB_CONTAINER'])
+blob_container = app.config['BLOB_CONTAINER']
+blob_service = BlockBlobService(account_name=app.config['BLOB_ACCOUNT'], account_key=app.config['BLOB_STORAGE_KEY'])
 
 def id_generator(size=32, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
@@ -54,32 +53,17 @@ class Post(db.Model):
         self.user_id = userId
 
         if file:
-            filename = secure_filename(file.filename)
-            fileextension = filename.rsplit('.', 1)[-1] if '.' in filename else ''
-            Randomfilename = id_generator()
-            filename = f"{Randomfilename}.{fileextension}" if fileextension else Randomfilename
-
+            filename = secure_filename(file.filename);
+            fileextension = filename.rsplit('.',1)[1];
+            Randomfilename = id_generator();
+            filename = Randomfilename + '.' + fileextension;
             try:
-                # Upload new file with proper content type
-                blob_client = blob_container_client.get_blob_client(filename)
-                blob_client.upload_blob(
-                    file,
-                    overwrite=True,
-                    content_settings=ContentSettings(content_type=file.mimetype)
-                )
-
-                # Delete old file if exists
-                if self.image_path:
-                    try:
-                        old_blob_client = blob_container_client.get_blob_client(self.image_path)
-                        old_blob_client.delete_blob()
-                    except Exception:
-                        pass
-            except Exception as e:
-                flash(str(e))
-
-            self.image_path = filename
-
+                blob_service.create_blob_from_stream(blob_container, filename, file)
+                if(self.image_path):
+                    blob_service.delete_blob(blob_container, self.image_path)
+            except Exception:
+                flash(Exception)
+            self.image_path =  filename
         if new:
             db.session.add(self)
         db.session.commit()
