eb665651f46f896d8f1f04c11a0a69a428ed1166
deekshithashetty746-ctrl <deekshithashetty746@gmail.com>
2025-09-13 19:07:05 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1,11 +1,7 @@
-"""
-This script runs the FlaskWebProject application using a development server.
-"""
-
 from os import environ
 from FlaskWebProject import app
 
 if __name__ == '__main__':
-    host = '0.0.0.0'  # Azure requires 0.0.0.0
-    port = int(environ.get('PORT', 5000))  # Azure sets PORT automatically
-    app.run(host=host, port=port)  # No SSL context needed; Azure handles HTTPS
+    host = '0.0.0.0'  # Must be 0.0.0.0 for Azure
+    port = int(environ.get('PORT', 5000))  # Azure automatically sets PORT
+    app.run(host=host, port=port)  # Remove ssl_context
