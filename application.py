46ab9bdca3802803fc58f333b9cc8f4afa1a0ba5
deekshithashetty746-ctrl <deekshithashetty746@gmail.com>
2025-09-13 19:12:41 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1,7 +1,11 @@
+
 from os import environ
 from FlaskWebProject import app
 
 if __name__ == '__main__':
-    host = '0.0.0.0'  # Must be 0.0.0.0 for Azure
-    port = int(environ.get('PORT', 5000))  # Azure automatically sets PORT
-    app.run(host=host, port=port)  # Remove ssl_context
+    HOST = environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(environ.get('SERVER_PORT', '5555'))
+    except ValueError:
+        PORT = 5555
+    app.run(HOST, PORT, ssl_context='adhoc')
